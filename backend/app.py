b19e75cb0113e856aa62410df24ff22664f4b3e1
Jonas Mota <jons.mota@gmail.com>
2025-01-30 16:13:23 -0300

    Atualizado configuração para deploy na Vercel

--- backend/app.py
+++ backend/app.py
@@ -40,4 +40,5 @@ def upload_file():
     return render_template('result.html', category=category, response=response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=os.environ.get('PORT', 5000))
